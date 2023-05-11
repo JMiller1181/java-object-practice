2110cd3c3036ab805bf38c702e095d678e59513e
Jacob Miller <millerjr1181@gmail.com>
2023-05-11 17:41:58 -0400

    Completed bank account exercises

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
 public class BankAccount {
     private double balance;
     private String name;
     private int accountNumber;
-//        public BankAccount(){
-//            this.balance = balance;
-//            this.name = name;
-//        }
+        public BankAccount(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("What is your name: ");
+            name = scanner.nextLine();
+            System.out.print("What is the starting balance: ");
+            balance = scanner.nextDouble();        }
         public BankAccount(double balance, String name,int accountNumber){
             this.balance = balance;
             this.name = name;
@@ -14,12 +17,29 @@ public class BankAccount {
         public void deposit(double deposit){
             balance += deposit;
         }
-        public void withdrawl(double withdrawl){
-            balance -= withdrawl;
+        public void withdrawal(double withdrawal){
+            balance -= withdrawal;
         }
-        public void getAccountInfo(){
-            System.out.println("The account holder is: " + name);
-            System.out.println("The account balance is: " + balance);
+        @Override public String toString(){
+            return "The account holder is: " + name + "\n" +
+                    "The account balance is: " + balance + "\n" +
+                    "The account number is: " + accountNumber;
+        }
+        public int getAccountNumber() {
+        return accountNumber;
         }
 
+        public void setAccountNumber(int accountNumber) {
+            this.accountNumber = accountNumber;
+        }
+        public String getName() {
+            return name;
+        }
+        public double getBalance() {
+            return balance;
+        }
+        public void transfer(BankAccount account, double transferAmount) {
+            balance -= transferAmount;
+            account.deposit(transferAmount);
+        }
 }
